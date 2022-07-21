b57cdc6329ae67e9bc2920921ff9e39ef2e4862c
Ngo Xuan Hieu <92346485+xhiew@users.noreply.github.com>
2022-07-21 10:10:37 +0700

    Initial Commit

---
+++ PassData_Closures/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PassData_Closures
+//
+//  Created by ADMIN on 21/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
